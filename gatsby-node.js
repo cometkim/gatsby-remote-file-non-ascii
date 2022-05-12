7bc12d577c78a3f26ffce14153ef0d6ed10e410c
Hyeseong Kim <hey@hyeseong.kim>
2022-05-12 10:59:58 +0900

    add a minimal reproduction

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const { createRemoteFileNode } = require('gatsby-source-filesystem');
+
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions
   createPage({
@@ -6,4 +8,37 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
+}
+
+exports.createResolvers = ({
+  createResolvers,
+  cache,
+  store,
+  reporter,
+  actions: {
+    createNode,
+  },
+  createNodeId,
+}) => {
+
+  // a340d817-7e76-4dfd-a704-74b4a526d77c_테스트.txt
+  const url = 'https://prismic-io.s3.amazonaws.com/karrot/a340d817-7e76-4dfd-a704-74b4a526d77c_%e1%84%90%e1%85%a6%e1%84%89%e1%85%b3%e1%84%90%e1%85%b3.txt';
+
+  createResolvers({
+    SiteSiteMetadata: {
+      test: {
+        type: 'File!',
+        resolve() {
+          return createRemoteFileNode({
+            url,
+            cache,
+            createNode,
+            createNodeId,
+            store,
+            reporter,
+          });
+        },
+      },
+    },
+  });
 }
